f80513db7aa81e90cd7cfa44292da261b0131dae
Roman Arekeev <roman.arekeev@appswithlove.com>
2019-11-20 11:22:12 +0400

    add helper classes to work with Configuration

---
+++ src/Api/Common/Constants/EnvironmentVariableNames.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Common.Constants
+{
+    public static class EnvironmentVariableNames
+    {
+        public static string AwsElasticsearchAccessKey = "AWS_ELASTICSEARCH_ACCESS_KEY";
+
+        public static string AwsElasticsearchSecretKey = "AWS_ELASTICSEARCH_SECRET_KEY";
+
+        public static string AwsRegionEndpoint = "AWS_REGION_ENDPOINT";
+
+        public static string AwsElasticsearchConnectionString = "AWS_ELASTICSEARCH_CONNECTION_STRING";
+    }
+}
---
+++ src/Api/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Api.Common.Constants;
+using Microsoft.Extensions.Configuration;
+
+namespace Api.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetAwsElasticsearchAccessKey(this IConfiguration configuration) =>
+            configuration?[EnvironmentVariableNames.AwsElasticsearchAccessKey]?.Trim() ?? string.Empty;
+
+        public static string GetAwsElasticsearchSecretKey(this IConfiguration configuration) =>
+            configuration?[EnvironmentVariableNames.AwsElasticsearchSecretKey]?.Trim() ?? string.Empty;
+
+        public static string GetAwsElasticsearchRegionEndpointName(this IConfiguration configuration) =>
+            configuration?[EnvironmentVariableNames.AwsRegionEndpoint]?.Trim() ?? string.Empty;
+
+        public static string GetAwsElasticsearchConnectionString(this IConfiguration configuration) =>
+            configuration?[EnvironmentVariableNames.AwsElasticsearchConnectionString]?.Trim() ?? string.Empty;
+    }
+}
