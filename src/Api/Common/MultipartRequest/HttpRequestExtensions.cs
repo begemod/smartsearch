4b0933c7b60787ebc3c2138388055452926ff33c
Roman Arekeev <roman.arekeev@appswithlove.com>
2019-11-21 09:30:22 +0400

    add helper classes to work with multipart requests

---
+++ src/Api/Common/MultipartRequest/HttpRequestExtensions.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Net.Http.Headers;
+using MimeTypes.Core;
+
+namespace Api.Common.MultipartRequest
+{
+    public static class HttpRequestExtensions
+    {
+        private static readonly FormOptions DefaultFormOptions = new FormOptions();
+
+        public static async Task<FormValueProvider> FetchFormData(
+            this HttpRequest request,
+            Stream[] targetStreams,
+            CancellationToken cancellationToken)
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(request.ContentType))
+            {
+                throw new Exception($"Expected a multipart request, but got {request.ContentType}");
+            }
+
+            // Used to accumulate all the form url encoded key value pairs in the request.
+            var formAccumulator = new KeyValueAccumulator();
+
+            var boundary = MultipartRequestHelper.GetBoundary(MediaTypeHeaderValue.Parse(request.ContentType), DefaultFormOptions.MultipartBoundaryLengthLimit);
+
+            var reader = new MultipartReader(boundary, request.Body);
+
+            var section = await reader.ReadNextSectionAsync(cancellationToken);
+
+            var filesCount = 0;
+
+            while (section != null)
+            {
+                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition, out var contentDisposition);
+
+                if (hasContentDispositionHeader)
+                {
+                    if (MultipartRequestHelper.HasFileContentDisposition(contentDisposition))
+                    {
+                        if (filesCount < targetStreams.Length)
+                        {
+                            await section.Body.CopyToAsync(targetStreams[filesCount], cancellationToken);
+
+                            var fieldName = contentDisposition.Name;
+                            formAccumulator.Append($"contentType_{fieldName}", MimeTypeMap.GetMimeType(Path.GetExtension(contentDisposition.FileName.Value)));
+                            formAccumulator.Append($"fileName_{fieldName}", contentDisposition.FileName.Value);
+
+                            filesCount++;
+                        }
+                    }
+                    else if (MultipartRequestHelper.HasFormDataContentDisposition(contentDisposition))
+                    {
+                        var key = HeaderUtilities.RemoveQuotes(contentDisposition.Name);
+                        var encoding = MultipartRequestHelper.GetEncoding(section);
+
+                        using (var streamReader = new StreamReader(
+                            section.Body,
+                            encoding,
+                            true,
+                            1024,
+                            true))
+                        {
+                            var value = await streamReader.ReadToEndAsync();
+
+                            if (string.Equals(value, "undefined", StringComparison.OrdinalIgnoreCase))
+                            {
+                                value = string.Empty;
+                            }
+
+                            formAccumulator.Append(key.Value, value);
+
+                            if (formAccumulator.ValueCount > DefaultFormOptions.ValueCountLimit)
+                            {
+                                throw new InvalidDataException($"Form key count limit {DefaultFormOptions.ValueCountLimit} exceeded.");
+                            }
+                        }
+                    }
+                }
+
+                section = await reader.ReadNextSectionAsync(cancellationToken);
+            }
+
+            foreach (var targetStream in targetStreams)
+            {
+                // Set position to 0 so stream is able to read
+                targetStream.Seek(0, SeekOrigin.Begin);
+            }
+
+            var formValueProvider = new FormValueProvider(
+                BindingSource.Form,
+                new FormCollection(formAccumulator.GetResults()),
+                CultureInfo.CurrentCulture);
+
+            return formValueProvider;
+        }
+    }
+}
---
+++ src/Api/Common/MultipartRequest/MultipartRequestHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Net.Http.Headers;
+
+namespace Api.Common.MultipartRequest
+{
+    public static class MultipartRequestHelper
+    {
+        public static Encoding GetEncoding(MultipartSection section)
+        {
+            var hasMediaTypeHeader = MediaTypeHeaderValue.TryParse(section.ContentType, out var mediaType);
+
+            if (!hasMediaTypeHeader || Encoding.UTF7.Equals(mediaType.Encoding))
+            {
+                return Encoding.UTF8;
+            }
+
+            return mediaType.Encoding;
+        }
+
+        // Content-Type: multipart/form-data; boundary="----WebKitFormBoundarymx2fSWqWSd0OxQqq"
+        public static string GetBoundary(MediaTypeHeaderValue contentType, int lengthLimit)
+        {
+            var boundary = HeaderUtilities.RemoveQuotes(contentType.Boundary);
+
+            if (string.IsNullOrWhiteSpace(boundary.Value))
+            {
+                throw new InvalidDataException("Missing content-type boundary.");
+            }
+
+            if (boundary.Length > lengthLimit)
+            {
+                throw new InvalidDataException($"Multipart boundary length limit {lengthLimit} exceeded.");
+            }
+
+            return boundary.Value;
+        }
+
+        public static bool IsMultipartContentType(string contentType)
+        {
+            return !string.IsNullOrEmpty(contentType)
+                   && contentType.IndexOf("multipart/", StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+
+        public static bool HasFormDataContentDisposition(ContentDispositionHeaderValue contentDisposition)
+        {
+            // Content-Disposition: form-data; name="key";
+            return contentDisposition != null
+                   && contentDisposition.DispositionType.Equals("form-data", StringComparison.CurrentCultureIgnoreCase)
+                   && string.IsNullOrEmpty(contentDisposition.FileName.Value)
+                   && string.IsNullOrEmpty(contentDisposition.FileNameStar.Value);
+        }
+
+        public static bool HasFileContentDisposition(ContentDispositionHeaderValue contentDisposition)
+        {
+            // Content-Disposition: form-data; name="myfile1"; filename="Misc 002.jpg"
+            return contentDisposition != null
+                   && contentDisposition.DispositionType.Equals("form-data", StringComparison.CurrentCultureIgnoreCase)
+                   && (!string.IsNullOrEmpty(contentDisposition.FileName.Value)
+                       || !string.IsNullOrEmpty(contentDisposition.FileNameStar.Value));
+        }
+    }
+}
