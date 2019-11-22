7c0cede8a62495358fcb5f14440b650e4d6a5a64
Roman Arekeev <roman.arekeev@appswithlove.com>
2019-11-22 19:53:00 +0400

    add SearchController

---
+++ src/Api/Features/Search/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Features.Search
+{
+    [Produces("application/json")]
+    [Route("api/search")]
+    public class SearchController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public SearchController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(
+            [FromQuery] string phrase,
+            [FromQuery] string markets,
+            [FromQuery] int? limit, CancellationToken cancellationToken)
+        {
+            return Ok(await _mediator.Send(new GetAll.Query
+            {
+                Phrase = phrase,
+                Markets = markets,
+                Limit = limit ?? 25
+            }, cancellationToken));
+        }
+    }
+}
